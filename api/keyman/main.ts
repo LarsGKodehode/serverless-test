c03b2fe8c9dacf924af90e6f5ad286bea6c70d44
larsg <lars.gunnar@jobloop>
2023-04-27 16:35:22 +0200

    setup basic middleman

---
+++ api/keyman/main.ts
@@ -0,0 +1,33 @@
+/**
+ * This acts like a middle man keeping our key secret from the frontend
+ */
+
+// This is the secret we want to keep from the frontend
+const API_KEY = ""
+
+// We have the base URL here since we will not change it in the code
+const BASE_URL = "https://finnhub.io/api/v1"
+
+const handler: any = {
+  async fetch(request: Request) {
+
+    // Setup headers
+    const header = new Headers()
+    header.append("X-Finnhub-Token", API_KEY)
+
+    // Setup query
+    const query = "/search?q=apple"
+
+    // Do fetch and wait for response
+    const res = await fetch(BASE_URL + query, { headers: header })
+
+    // Pass response along to frontend
+    return new Response(res.body, {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  },
+};
+
+export default handler;
